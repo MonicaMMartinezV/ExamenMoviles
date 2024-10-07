4f1446bc9cc9f7561a5da4861f313580a5a73725
MonicaMMartinezV <A01710965@tec.mx>
2024-10-07 09:18:40 -0600

    Feature: Agregar lo que sea

---
+++ app/src/main/java/com/example/kotlin/examenmoviles/domain/Requirement.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.examenmoviles.domain
+
+import com.example.kotlin.examenmoviles.data.Repository
+
+class Requirement {
+//    private val repository = Repository()
+//
+//    suspend operator fun invoke(
+//        numberItem:Int
+//    ): //??
+}
